1e7d1cb71d3b4083d9d087fcf8c5a83b0d1f18df
babu-hariharan <babuhariharanmaticz@gmail.com>
2024-09-16 11:59:21 +0530

    added_frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,32 @@ import './assets/css/Style.css'
 
 const App = () => {
   return (
-    <div style={{ color: "red" }} >Apasfeafp</div>
+    <div className='task'>
+      <div className='sidetab'>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+      </div>
+      <div className='banner_section'>
+        <div className='container h-100  px-0'>
+          <div className='content_container'>
+            <div className='row h-100 align-items-center'>
+              <div className='col-6'>
+                <p className='title'>
+                  Revolution your Home Owner
+                </p>
+                <p className='sub_title '>
+                  Discover a new era of property investment with HomeCubes. Our Web3-enabled platform leverages blockchain technology and fractional ownership to make real estate accessible for everyone. Enjoy the benefits of our unique staking model, which offers progressive income through rental and commission rewards. Experience hassle-free management and transparent investments, all through a user-friendly app.
+                </p>
+              </div>
+               <div className='col-6'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
